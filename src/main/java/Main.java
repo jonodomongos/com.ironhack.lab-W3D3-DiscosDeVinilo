62f29dd60ae5cf73a1b0fee30d763dd29976fc92
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-20 00:43:58 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        INSTRUCCIONES:
+        Crear un arraylist con al menos 5 discos, con los datos: Artista, título, año, duración (pueden ser otros).
+            - Mostrar todos los discos, con todos los datos
+            - Mostrar uno determinado: por ejemplo el año del tercer disco (esto lo podría pedir el cliente, o no)
+            - Cambiar ese mismo año (esto lo podría pedir el cliente, o no) y mostrar ese cambio. A ver si lo encontráis!
+            - Etc hacer pruebas
+
+        EXTRA: buscador con contains() o con equals(). Que el usuario escriba nombre de artista y me diga si está, y en qué posición está.
+
+        NIVEL 2
+        Que todos estos datos los entre el cliente: usuario entra Artista, título, año y duración (no hace falta hacer un CRUD entero).
+         */
+    }
+}
